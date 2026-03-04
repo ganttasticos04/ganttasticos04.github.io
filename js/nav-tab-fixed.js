d531c187ceddc4f35397586d78d7a5819dc114e9
AngelMendieta <jesusangelmendieta@gmail.com>
2026-03-03 23:49:13 -0600

    Version 1.06 - Nuevo correo

---
+++ js/nav-tab-fixed.js
@@ -0,0 +1,45 @@
+import { resaltaSiEstasEn } from "./lib/resaltaSiEstasEn.js"
+
+export class NavTabFixed extends HTMLElement {
+
+ constructor() {
+  super()
+  this.creado = false
+ }
+
+ connectedCallback() {
+  this.classList.add("md-tab", "fixed")
+
+  if (!this.creado) {
+
+   this.innerHTML = /* HTML */`
+    <a ${resaltaSiEstasEn(["/index.html", "", "/"])} href="index.html">
+     <span class="material-symbols-outlined">my_location</span>
+     Geo
+    </a>
+ 
+    <a ${resaltaSiEstasEn(["/archivos.html"])} href="archivos.html">
+     <span class="material-symbols-outlined">folder_open</span>
+     Archivos
+    </a>
+
+    <a ${resaltaSiEstasEn(["/camara.html"])} href="camara.html">
+     <span class="material-symbols-outlined">photo_camera</span>
+    Camara
+    </a>
+
+    <a ${resaltaSiEstasEn(["/ayuda.html"])} href="ayuda.html">
+     <span class="material-symbols-outlined">help</span>
+     Ayuda
+    </a>`
+
+
+   this.creado = true
+
+  }
+
+ }
+
+}
+
+customElements.define("nav-tab-fixed", NavTabFixed)
